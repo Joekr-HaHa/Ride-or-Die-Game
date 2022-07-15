12dfa6e6f60aa2b2e98759f7bad9d0debd97618d
Joekr-HaHa <109162427+Joekr-HaHa@users.noreply.github.com>
2022-07-15 17:48:58 +0100

    Pause Menu

--- 3dattempt/Player.gd
+++ 3dattempt/Player.gd
@@ -47,7 +47,8 @@ func _physics_process(delta):
 		grounded = false
 		gravity_vec = Vector3.UP * jump
 	if Input.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		pass
 	if $Head/RayCast.is_colliding():
 		InteractLabel.set_visible(true)
 	if Input.is_action_pressed("interact") and $Head/RayCast.is_colliding():
