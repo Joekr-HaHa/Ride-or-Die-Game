4c0f2d0b6c41431fad6e9b01ac3b2ff29e063f80
Joekr-HaHa <109162427+Joekr-HaHa@users.noreply.github.com>
2022-07-15 20:17:21 +0100

    Added reticle and enemy

---
+++ 3dattempt/Enemy.gd
@@ -0,0 +1,20 @@
+extends KinematicBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var material = SpatialMaterial.new()
+
+	material.albedo_color = "red"
+
+	self.set_material(material) # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
