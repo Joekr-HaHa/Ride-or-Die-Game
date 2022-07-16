e82e9bbc5a48814973c7a9465ba78e02df956eef
Lasrixx <olivia@grayworld.co.uk>
2022-07-16 18:28:11 +0100

    strong player done

--- 3dattempt/Scripts/Crate.gd
+++ 3dattempt/Scripts/Crate.gd
@@ -1,14 +1,2 @@
-extends StaticBody
+extends RigidBody
 
-func set_parent():
-	var new_parent
-	if get_parent().name == "Test":
-		# Set to player
-		new_parent = get_tree().get_node("PlayerGrab")
-	else:
-		# Set to test 
-		new_parent = get_tree().get_node("Test")
-	# Remove self from current parent
-	get_parent().remove_child(self)
-	# Add self to new_parent
-	new_parent.add_child(self)
--- 3dattempt/Scripts/PlayerGrab.gd
+++ 3dattempt/Scripts/PlayerGrab.gd
@@ -15,6 +15,10 @@ var movement = Vector3()
 var gravity_vec = Vector3()
 var grounded = true
 
+var carrying = false
+var canCarry = true
+onready var crate = preload("res://Scenes/ThrowableCrate.tscn")
+
 onready var InteractLabel=get_node("UI/InteractLabel")
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -64,9 +68,25 @@ func _physics_process(delta):
 	if Input.is_action_pressed("use_ability") and $Head/InteractRayCast.is_colliding():
 		# Move crate with player 
 		var collider = $Head/InteractRayCast.get_collider()
-		if collider.name == "Crate":
-			add_child(collider)
-	
+		if collider.is_in_group("Crate") and canCarry == true:
+			carrying = true
+			collider.queue_free()
+			canCarry = false
+			yield(get_tree().create_timer(0.5),"timeout")
+			canCarry = true
+			#collider.set_parent()
+	if Input.is_action_pressed("use_ability") and carrying == true and canCarry == true:
+		# Drop the box
+		carrying = false	
+		var crateInstance = crate.instance()
+		owner.add_child(crateInstance)
+		crateInstance.global_transform = get_node("CrateSpawnPoint").global_transform
+		
+	if carrying == true:
+		get_node("Box").visible = true
+	else:
+		get_node("Box").visible = false
+		
 	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
 	movement.z = velocity.z + gravity_vec.z
 	movement.x = velocity.x + gravity_vec.x
